009408682b0266031e12495e53c87d6994cacf7b
Danilo Cunha <danilokage@gmail.com>
2019-04-26 20:54:08 -0300

    Created static method to get data from update

--- bot.py
+++ bot.py
@@ -5,8 +5,10 @@ import requests
 class Telegram:
 
     def __init__(self):
-        self.name = ''
+
         self.url = "https://api.telegram.org/bot{}/".format(BOT_KEY)
+
+        self.name = ''
         self.bot_id = 0
         self.offset = None
         self.aux = 0
@@ -35,27 +37,35 @@ class Telegram:
         # Grabs last offset update_id, and adds one so updates already received and not returned anymore
         self.offset = int(json_response['result'][-1]['update_id']) + 1
         if json_response['ok']:
-            return True
+            return json_response['result']
         else:
             return False
 
-    def message_treatement(self):
+    @staticmethod
+    def get_update_data(json_update):
+
+        update_text = json_update['text']
+        update_chat_id = json_update['from']['id']
+        update_from_user = json_update['from']['username']
+
+        if not update_text or not update_chat_id:
+            return False
+        else:
+            return update_text, update_chat_id, update_from_user
+
+    def get_last_update_id(self):
         # TODO
-        self.aux = 'aux3'
-        return True
+        pass
 
-    def get_last_update_ip(self):
-        # TODO
-        self.aux = 'aux3'
-        return True
-
-    def send_message(self, message, chat_id):
-
-        return True
+    def send_message(self, text_to_send, receiver_id):
+        pass
 
 
 bot = Telegram()
 
 bot.get_me()
-bot.get_updates()
-# bot.get_updates()
+json_return = bot.get_updates()
+message, chat, username = bot.get_update_data(json_return[0]['message'])
+print(message)
+print(chat)
+print(username)
